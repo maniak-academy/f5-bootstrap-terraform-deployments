dda317aa608e6e30178792aa2bc934888adc06c1
Sebbycorp <sebastian@maniak.io>
2022-04-26 17:50:01 +0000

    updates

--- bigip_main.tf
+++ bigip_main.tf
@@ -7,7 +7,7 @@ If a copy of the MPL was not distributed with this file, You can obtain one at h
 terraform {
   required_providers {
     bigip = {
-      source = "F5Networks/bigip"
+      source  = "F5Networks/bigip"
       version = "1.13.1"
     }
   }
@@ -15,7 +15,7 @@ terraform {
 
 
 provider "bigip" {
-  address = "192.168.86.33"
+  address  = "192.168.86.33"
   username = "admin"
   password = "W3lcome098!"
 }
@@ -29,24 +29,30 @@ resource "null_resource" "install_do" {
   }
 }
 
+resource "time_sleep" "wait_30_seconds" {
+  depends_on = [null_resource.install_do]
+
+  create_duration = "15s"
+}
+
 
 data "template_file" "init" {
   template = file("bigip.tpl")
   vars = {
-    HOSTNAME = "bigip.maniak.academy"
-    DNS_ADDRESS = "8.8.8.8"
-    NTP_ADDRESS = "8.8.8.8"
+    HOSTNAME       = "bigip.maniak.academy"
+    DNS_ADDRESS    = "8.8.8.8"
+    NTP_ADDRESS    = "8.8.8.8"
     GUEST_PASSWORD = "W3lcome098!"
   }
 }
-resource "bigip_do"  "do-deploy" {
-    do_json = data.template_file.init.rendered
-    #depends_on = [null_resource.install_do]
+resource "bigip_do" "do-deploy" {
+  do_json    = data.template_file.init.rendered
+  depends_on = [time_sleep.wait_30_seconds]
 }
 
 resource "null_resource" "install_as3" {
   provisioner "local-exec" {
     command = "./install-as3-rpm.sh 192.168.86.33 admin:W3lcome098!"
-    depends_on = [bigip_do.do-deploy]
   }
+  depends_on = [bigip_do.do-deploy]
 }
